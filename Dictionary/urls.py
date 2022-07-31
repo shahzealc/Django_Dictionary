d1517b78a2d610f6a29610f312decf6075f77965
unknown <zeal532003@gmail.com>
2022-07-31 18:22:27 +0530

    Commit

--- Dictionary/urls.py
+++ Dictionary/urls.py
@@ -17,6 +17,9 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import path ,include
 
+from django.views.static import serve
+from django.conf.urls import url
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
